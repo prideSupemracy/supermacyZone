600a3791e66e852294ccb635154458388b05bdf0
kumargauravmishra <kumar.gaurav@hirepro.in>
2016-03-04 18:04:18 +0530

    apply() method invocation added

--- prototype.js
+++ prototype.js
@@ -14,10 +14,22 @@ var myFun = () => {
 };
 
 */ 
-var MY_FUN = function(){
-	this.state = "below";
+var MY_FUN = function(string){
+	this.state = string;
 };
-MY_FUN.prototype.getStatus = () => this.state = "above";
-var fun = new MY_FUN();
+MY_FUN.prototype.getStatus = function(){return this.state};
+var fun = new MY_FUN("Kumar");
 console.log("Prototyped function called : " ,fun.getStatus());
 console.log("main state of the function accessed : " , fun.state);
+// apply() method and it's invocation
+var add = function (a, b) {
+return a + b;
+};
+var ob = {
+	state : "EveryThing is ok"
+};
+var arr = [10, 29.99];
+var sum = add.apply(null,arr);
+console.log("Sum of the array : " , sum);
+var status = MY_FUN.prototype.getStatus.apply(ob);
+console.log(status);
